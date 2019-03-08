2bf4ed1eddd2bce26a1468cd52725c7759c95dd6
autom8ter <admin@autom8ter.com>
2019-03-07 22:43:14 -0700

    funcs

--- config.go
+++ config.go
@@ -1,14 +1,1 @@
 package identify
-
-import (
-	"github.com/autom8ter/identify/driver"
-	"github.com/volatiletech/authboss"
-)
-
-func NewConfig(opts ...driver.ConfigFunc) *authboss.Config {
-	cfg := &authboss.Config{}
-	for _, o := range opts {
-		o(cfg)
-	}
-	return cfg
-}
